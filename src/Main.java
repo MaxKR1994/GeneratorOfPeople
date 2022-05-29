e6f46e2e05f47a6fd8c0bc3f74c4685b47a13402
MaxKR1994 <98046645+MaxKR1994@users.noreply.github.com>
2022-05-29 11:01:04 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        GeneratorOfPeople[] human = GeneratorOfPeople.createHumans(GeneratorOfPeople.randomName(),GeneratorOfPeople.randomSurname());
+    }
+}
